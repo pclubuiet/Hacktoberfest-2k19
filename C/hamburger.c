3927e393f249f0e751482f1c2211a65749510026
ankay212000 <nktumharadost21@gmail.com>
2019-10-02 21:23:15 +0530

    resolve issue#68

---
+++ C/hamburger.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<11;j++)
+        {
+            if(i==2)
+            printf("1");
+            else
+            printf("0");
+            
+        }
+        printf("\n");
+    }
+
+}
