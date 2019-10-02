d90824ad82ef3400878729cf4968a03a8c2678c0
Keysang Yonthan <keysangkeysang@gmail.com>
2019-10-02 12:48:38 +0530

    added a progam to add 2 numbers

---
+++ CPP/sum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num1,num2,sum;
+cout<<"Enter two numbers"<<endl;
+cin>>num1>>num2;
+sum=num1+num2;
+cout<<"\nThe sum of two numbers is "<<sum<<endl;
+return 0;
+}
