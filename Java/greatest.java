8925340bff7d93e94daa6d405072cf2396507331
nikhil0506012 <nikhil0506012@gmail.com>
2019-10-02 14:08:39 +0530

    Changes by Nikhil Kumar

---
+++ Java/greatest.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class greatest {
+    public static void main(String[] args) {
+        Scanner key = new Scanner(system.in);
+        System.out.println("enter two numbes to compare");
+        int a = key.nextInt() , b = key.nextInt();
+        System.out.println( a>b?a:b );
+    }
+}
