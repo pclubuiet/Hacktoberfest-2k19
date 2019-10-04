1d8e5a78346a0d3383c4980d0d5ac7d8d882c47d
unika-debug <unikamittal18@gmail.com>
2019-10-04 13:09:41 +0530

    closes #150 diamond pattern in c++

---
+++ pattern/pattern.cpp
@@ -0,0 +1,39 @@
+/* C++ Program - Print Diamond Pattern */
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    int n, c, k, space=1;
+    cout<<"Enter number of rows (for diamond dimension) : ";
+    cin>>n;
+    space=n-1;
+    for (k=1; k<=n; k++)
+    {
+	for(c=1; c<=space; c++)
+	{
+		cout<<" ";
+	}
+	space--;
+	for(c=1; c<=(2*k-1); c++)
+	{
+		cout<<"*";
+	}
+	cout<<"\n";
+    }
+    space=1;
+    for(k=1; k<=(n-1); k++)
+    {
+	for(c=1; c<=space; c++)
+	{
+		cout<<" ";
+	}
+	space++;
+	for(c=1 ; c<=(2*(n-k)-1); c++)
+	{
+		cout<<"*";
+	}
+	cout<<"\n";
+    }
+    getch();
+}
