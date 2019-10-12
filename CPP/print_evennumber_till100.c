a8d40294bcfaab03cfc3b2765a4cf315e941c364
TanyaAggrawal <56471903+TanyaAggrawal@users.noreply.github.com>
2019-10-12 21:57:55 +0530

    added program to print even number till 100

---
+++ CPP/print_evennumber_till100.c
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+    int i;
+ 
+    /* Run a loop from 1 to 100. */
+ 
+    for(i = 1; i < = 100; i++){
+  
+       /* If number is divisible by 2. */
+  
+      if(i % 2 == 0) {
+
+         cout << i <<" "; 
+      }
+   }
+    return 0;
+}
